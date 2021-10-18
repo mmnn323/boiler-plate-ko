5cdfab99c1cb4c75d04e39925538051fc2b9c4a9
mmnn323 <82758086+mmnn323@users.noreply.github.com>
2021-10-18 10:51:36 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')  // express 모듈 가져옴
 const app = express()
 const port = 5001
+const {User} = require("./models/User");
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ryu:1234@boilerplate.59vtb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected')).catch(err =>console.log(err))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected')).catch(err =>console.log(err))
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!!!!!!')
+})
+
+app.post('/register',(req,res)=>{
+    // 회원가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
 })
 
 app.listen(port, () => {
